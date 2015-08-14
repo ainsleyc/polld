532a68eb6b634d531d9f43576d9c35f957e41d61
Ainsley Chong <ainsley@factual.com>
2015-08-14 07:29:44 -0700

    Converting to child process

--- test/run_test.js
+++ test/run_test.js
@@ -1,29 +1,26 @@
 'use strict';
 
 var assert = require('should');
-var sinon = require('sinon');
+// var sinon = require('sinon');
 
-var options = require('../lib/options');
+// var options = require('../lib/options');
 var taskRunner = require('../lib/taskRunner');
 
 var statsdStub = {};
-function emptyTask(statsd, cb) {
-  cb(null);
-}
+// function emptyTask(statsd, cb) {
+//   cb(null);
+// }
 
 describe('taskRunner', function () {
 
-  it('should call the task iteration number of times', function (done) {
-    var spy = sinon.spy(emptyTask);
-    taskRunner._startIntervals(spy, statsdStub, 10, 10, function(err, count) {
-      spy.callCount.should.be.equal(10);
-      done();
-    });
-  });
+  var taskfile = './example/task.js';
+  var options = {
+    host: 'localhost',
+    port: 8125
+  }
 
   it('should return number of iterations', function (done) {
-    var spy = sinon.spy(emptyTask);
-    taskRunner._startIntervals(emptyTask, statsdStub, 10, 10, function(err, count) {
+    taskRunner._startIntervals(taskfile, options, 10, 10, function(err, count) {
       count.should.be.equal(10);
       done();
     });
